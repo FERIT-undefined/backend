a13e02b6805b5268f2a656a0d43714348b0423bf
franjojosip <franjojosip.jukic2@gmail.com>
2020-11-26 23:39:06 +0100

    Add patch endpoint for users

--- src/users/routes.js
+++ src/users/routes.js
@@ -8,11 +8,13 @@ const register = require('./auth/register')
 const logout = require('./auth/logout')
 const remove = require('./remove/remove')
 const list = require('./list/list')
+const patch = require('./patch/patch')
 
 router.get('/', authenticateJWT, list);
 router.post('/register', validateRegisterUser, register);
 router.post('/login', validateLoginUser, login);
 router.post('/logout', logout);
 router.delete('/remove/:id', authenticateJWT, remove);
+router.patch('/:id', authenticateJWT, patch);
 
 module.exports = router;
