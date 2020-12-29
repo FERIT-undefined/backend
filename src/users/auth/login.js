8dc5abf41a9383d5cf564f80564440cc1718f441
Matej Arlović <matej.arlovic@gmail.com>
2020-12-29 09:41:37 +0100

    Refactored login system and added JOI validation.

--- src/users/auth/login.js
+++ src/users/auth/login.js
@@ -2,35 +2,47 @@ module.exports = login;
 const User = require("../schema");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Joi = require('joi');
+
+const serializer = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
 
 async function login(req, res) {
-  const user = await User.findOne({ email: req.body.email });
-  if (user) {
-    let passwordHash = user.password;
-    if (bcrypt.compareSync(req.body.password, passwordHash)) {
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: process.env.ACCESS_TOKEN_LIFE,
-      });
-      const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
-      user.refreshToken = refreshToken;
 
-      await user.save();
-
-      return res.json({
-        user:{
-          fname: user.fname,
-          lname: user.lname,
-          role: user.role,
-          accessToken,
-          refreshToken: user.refreshToken,
-        },
-      });
-    }
+  const result = serializer.validate(req.body);
+  if(result.error) {
+      return res.status(400).send(result.error);
   }
-  return res.status(401).json({ error: "Invalid user credentials" });
+
+  const user = await User.findOne({ email: result.value.email });
+  if (!user) return res.status(401).json({ error: "Invalid user credentials" });
+
+  const passwordHash = user.password;
+  if (bcrypt.compareSync(result.value.password, passwordHash)) {
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: process.env.ACCESS_TOKEN_LIFE,
+    });
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
+    user.refreshToken = refreshToken;
+
+    await user.save();
+    return res.json({
+      user:{
+        id: user.id,
+        fname: user.fname,
+        lname: user.lname,
+        role: user.role,
+        accessToken,
+        refreshToken: user.refreshToken
+      }
+    });
+  }  
 }
