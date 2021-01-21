51d35455a2d246a325fc39076051311143c6dde3
Matej Arlović <matej.arlovic@gmail.com>
2021-01-21 09:43:18 +0100

    Opened GET endpoint for getting menu meals from the database based on the meal's Id.

--- src/menu/list/index.js
+++ src/menu/list/index.js
@@ -1,4 +1,5 @@
 const Meal = require('../schema');
+const Joi = require('joi');
 
 async function listAllMeals(req, res) {
 
@@ -26,4 +27,37 @@ async function listAllMeals(req, res) {
     }
 }
 
-module.exports = listAllMeals;
+async function listByMealId(req, res) {
+
+    const validatorParams = Joi.object({
+        meal_id: Joi.string().required()
+    });
+    const resultParams = validatorParams.validate(req.params);
+
+    if(resultParams.error) {
+        return res.status(400).send(resultParams.error);
+    }
+    if(!resultParams.value.meal_id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(400).send({ error: 'Invalid ObjectId' });
+    }
+
+    let meal = await Meal.findById(resultParams.value.meal_id);
+    try {
+
+        if(!meal) return res.status(404).json({ error: 'There are no meals with that ID in the database' });
+        return res.status(200).json({ 
+            'name': meal.name,
+            'description': meal.description,
+            'type': meal.type,
+            'price': meal.price,
+            'pdv': meal.pdv,
+            'discount': meal.discount
+        });
+    }
+    catch(err) {
+        return res.status(500).json({ error: err });
+    }
+}
+
+exports.listAllMeals = listAllMeals;
+exports.listByMealId = listByMealId;
--- src/menu/routes.js
+++ src/menu/routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authenticateJWT = require('../_helpers/authenticateJWT');
 
-const listAllMeals = require('./list');
+const list = require('./list');
 const add = require('./add/index');
 const remove = require('./remove/index');
 const editMeal = require('./edit/index');
 
-router.get('/', listAllMeals);
+router.get('/', list.listAllMeals);
+router.get('/:meal_id', list.listByMealId);
 router.post('/add', authenticateJWT, add);
 router.delete('/remove/:id', authenticateJWT, remove);
 router.patch('/:id', authenticateJWT, editMeal);
